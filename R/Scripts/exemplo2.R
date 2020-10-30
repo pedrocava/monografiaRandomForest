e9eb70c13dd55ba5090c1b5588025b748635680a
Pedro Cavalcante Oliveira <pedrocolrj@gmail.com>
2020-10-30 17:21:41 -0300

    exemplo 2

---
+++ R/Scripts/exemplo2.R
@@ -0,0 +1,36 @@
+
+
+
+
+library(tidyverse)
+library(magrittr)
+library(wesanderson)
+library(stargazer)
+library(knitr)
+
+pal <- wes_palette("Cavalcanti1")
+cor <- wes_palette("Cavalcanti1") %>% 
+  pluck(1)
+
+
+(houses <- readRDS("R/Dados/houses_clean.Rds"))
+
+file.create("tabelas/tabela_arvore_reg.tex")
+
+tabela <- file("tabelas/tabela_arvore_reg.tex")
+
+houses %>%
+  group_by(cidade) %>%
+  summarise(across(area:aceita_animal, mean)) %>%
+  kable("latex") %>%
+  enc2utf8() %>%
+  writeLines(tabela, useBytes = TRUE)
+
+close(tabela)
+
+
+arvore <- rpart(aluguel ~ .,
+                data = houses)
+png("imagens/arvore_reg_casas.png")
+rpart.plot(arvore)
+dev.off()
