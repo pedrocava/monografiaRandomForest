e737d10d01ac734dd72f683cd2825bf783cbbd2e
Pedro Cavalcante <pedrocolrj@gmail.com>
2020-12-18 12:09:42 -0300

    tweaking cant stop wont stop

--- R/Scripts/exemplolinreg_nonlin_e_heterocedasticidade.R
+++ R/Scripts/exemplolinreg_nonlin_e_heterocedasticidade.R
@@ -16,7 +16,7 @@ cor <- wes_palette("Cavalcanti1") %>%
 
 (tibble(x = runif(1000, -10, 10),
         x2 = x^2,
-        y = 100 + 5*x - 2*x^2 + rnorm(length(x), sd = 20)) -> 
+        y = 100 + 2*x - 5*x^2 + rnorm(length(x), sd = 20)) -> 
   data)
 
 png("imagens/exemplo4_dist.png", width = 940, height = 678, res = 120)
@@ -24,7 +24,7 @@ data %>%
   ggplot(aes(x = x, y = y)) +
   geom_point(size = 2, color = cor, alpha = .4) +
   theme_minimal() +
-  labs(title = "y(x) = 100 + 5x - 2x^2 + resíduo",
+  labs(title = "y(x) = 100 + 2x - 5x^2 + resíduo",
        subtitle = "Resíduo aleatórios tirados de uma N(0, 20)",
        x = "X",
        y = "Y")
@@ -41,8 +41,8 @@ tidy(modelo1) %>%
     termo = term,
     estimativa = estimate,
     erro_padrao = std.error,
-    estatistica_t = statistic,
-    p_valor = p.value) %>%
+    estatistica_t = statistic) %>%
+  select(-p.value) %>%
   kable(format = 'latex',
         caption = 'Modelo sem termo quadrático',
         label = 'tabela1_exemplo4',
@@ -58,8 +58,8 @@ tidy(modelo2) %>%
     termo = term,
     estimativa = estimate,
     erro_padrao = std.error,
-    estatistica_t = statistic,
-    p_valor = p.value) %>%
+    estatistica_t = statistic) %>%
+  select(-p.value) %>%
   kable(format = 'latex',
         caption = 'Modelo com termo quadrático',
         label = 'tabela2_exemplo4',
